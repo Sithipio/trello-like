a39d133c1fac12ac0cbb7a214a93b7959bd204e4
Berezos Valentyn <valentyn.berezos@inventorsoft.co>
2022-10-07 16:30:53 +0300

    change readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://trello-like-back.herokuapp.com',
+  API_PUBLIC_URL: 'https://trello-like-back.herokuapp.com/auth'
 };
